c97b9bfaeb2723fcb94f250edb8a94091590b883
zhw <wushizhw@outlook.com>
2021-03-24 11:03:57 +0800

    新建jvm demo模块

---
+++ demo-jvm/src/main/java/com/hello/demo/jvm/Main.java
@@ -0,0 +1,12 @@
+package com.hello.demo.jvm;
+
+/**
+ * @author: zhaohw
+ * @date: 2021/3/24 11:02
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
