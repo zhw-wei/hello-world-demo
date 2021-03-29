6c7281f7b9462d86685cebcb138db655af9c7a97
zhw-wei <2261842924@qq.com>
2021-03-29 16:48:19 +0800

    iload_1, istore_1 本地变量表

--- demo-jvm/src/main/java/com/hello/demo/jvm/register/Main.java
+++ demo-jvm/src/main/java/com/hello/demo/jvm/register/Main.java
@@ -12,15 +12,16 @@ public class Main {
         main.test04();
     }
 
-    private void test01(){
+    private void test01() {
         System.out.println("test01");
         test02();
     }
-    private void test02(){
+
+    private void test02() {
         System.out.println("test01");
     }
 
-    private void test03(){
+    private void test03() {
         int a = 0;
         {
             int b = 0;
@@ -30,33 +31,51 @@ public class Main {
         int c = a + 1;
     }
 
-    private void test04(){
+    private void test04() {
         //第一类问题
         int i1 = 10;
         i1++;
 
         int i2 = 10;
         ++i2;
+    }
 
+    public void test05() {
+        /*
+         0 bipush 10
+         2 istore_1
+         3 iload_1
+         4 iinc 1 by 1  ==> inic 等同于执行了iload_1、istore_1两条命令，执行到此处时实际上做了两次iload_1
+         7 istore_2 ==> 取到的结果时第一次iload_1的值
+        */
         //第二类问题
         int i3 = 10;
         int i4 = i3++;
 
+        /*
+         8 bipush 10
+        10 istore_3
+        11 iinc 3 by 1 ==> iload_3、istore_3
+        14 iload_3  ==> 此处取到的结果是加一后的结果
+        15 istore 4
+        17 return
+         */
         int i5 = 10;
         int i6 = ++i5;
+    }
 
+    public void test06() {
         //第三类问题
         int i7 = 10;
-        i7 = i7 ++;
-        System.out.println(i7);//10
+        i7 = i7++;  //10
 
         int i8 = 10;
-        i8 = ++i8;
-        System.out.println(i8);//11
+        i8 = ++i8;  //11
+    }
 
+    public void test07() {
         //第四类问题
         int i9 = 10;
-        int i10 = i9++ + ++i9;
-        System.out.println(i10);//22
+        int i10 = i9++ + ++i9;//22
     }
 }
