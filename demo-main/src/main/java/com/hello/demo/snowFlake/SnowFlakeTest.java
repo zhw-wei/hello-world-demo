498b48d042729eae279811109febdc6488ef0b8b
zhw-wei <2261842924@qq.com>
2021-04-29 15:34:35 +0800

    add junit5

--- demo-main/src/main/java/com/hello/demo/snowFlake/SnowFlakeTest.java
+++ demo-main/src/main/java/com/hello/demo/snowFlake/SnowFlakeTest.java
@@ -1,14 +1,15 @@
 package com.hello.demo.snowFlake;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author: zhaohw
  * @date: 2021.04.21 下午 3:01
  * @description:
  */
-public class Main {
+public class SnowFlakeTest {
 
-    public static void main(String[] args) {
-
+    @Test
+    public void testCreateId(){
         System.out.println(SnowFlakeUtil.createId());
-    }
-}
+    }}
