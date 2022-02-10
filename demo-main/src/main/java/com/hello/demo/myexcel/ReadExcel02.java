c289acb6e1c0653c6e91fcd7a9a5952b51b9fe2f
zhaohw <zhaohw@segimail.com>
2022-02-10 17:53:38 +0800

    修改导出结果

--- demo-main/src/main/java/com/hello/demo/myexcel/ReadExcel02.java
+++ demo-main/src/main/java/com/hello/demo/myexcel/ReadExcel02.java
@@ -1,6 +1,5 @@
 package com.hello.demo.myexcel;
 
-import com.alibaba.fastjson.JSON;
 import com.hello.demo.myexcel.excelv2.CreateExcelV2;
 import com.hello.demo.myexcel.excelv2.ExcelInfo;
 import com.hello.demo.util.ExcelUtil;
@@ -24,37 +23,28 @@ import java.util.stream.Collectors;
  * @date: 2022.02.09 下午 2:49
  */
 public class ReadExcel02 {
+    static String TOTAL_PATH = "D:/document/2022-02/合同信息02/合同信息.xlsx";
     static int TOTAL_START = 0;
-    static int TOTAL_END = 22;
+    static int TOTAL_END = 32;
 
+    static String USER_PATH = "D:/document/2022-02/合同信息02/签署方信息.xlsx";
+    static int USER_START = 100;
+    static int USER_END = 105;
+
+    static String ATTR_PATH = "D:/document/2022-02/合同信息02/合同属性信息.xlsx";
     static int ATTR_START = 201;
-    static int ATTR_END = 227;
+    static int ATTR_END = 221;
 
-    public static void main(String[] args) throws Exception {
-        List<ContractExcelInfo> contractList = read_total();
-        System.out.println("原始值： " + contractList);
-
-        List<UserInfo> userList = read_write_user();
-        Map<String, UserInfo> userMap = userList.stream().collect(Collectors.toMap(var0 -> var0.getCttId(),
+    private static void objectCopy(List<ContractExcelInfo> toObjectList, List<ContractExcelInfo> fromObjectList,
+                                   int startIndex, int endIndex) throws Exception{
+        Map<String, ContractExcelInfo> fromMap = fromObjectList.stream().collect(Collectors.toMap(var0 -> var0.getVal0(),
                 Function.identity(), (var1, var2) -> var1));
-        System.out.println("相对方: " + userList);
 
-        for (ContractExcelInfo info : contractList) {
-            UserInfo userInfo = userMap.getOrDefault(info.getVal0(), new UserInfo());
-            info.setVal100(userInfo.getUserTypeName());
-            info.setVal101(userInfo.getUserTypeName2());
-        }
-
-        List<ContractExcelInfo> attrList = read_attr_info();
-        Map<String, ContractExcelInfo> attrMap = attrList.stream().collect(Collectors.toMap(var0 -> var0.getVal0(),
-                Function.identity(), (var1, var2) -> var1));
-        System.out.println("属性值： " + attrList);
-
-        for (ContractExcelInfo info : contractList) {
-            ContractExcelInfo attrInfo = attrMap.get(info.getVal0());
+        //对象复制
+        for (ContractExcelInfo info : toObjectList) {
+            ContractExcelInfo attrInfo = fromMap.get(info.getVal0());
             if (Objects.nonNull(attrInfo)) {
-                //22~39
-                for (int i = ATTR_START; i <= ATTR_END; i++) {
+                for (int i = startIndex; i <= endIndex; i++) {
                     String param = String.format("val%s", i);
                     PropertyDescriptor descriptor = new PropertyDescriptor(param, ContractExcelInfo.class);
                     String value = String.valueOf(descriptor.getReadMethod().invoke(attrInfo));
@@ -62,7 +52,25 @@ public class ReadExcel02 {
                 }
             }
         }
+    }
 
+    public static void main(String[] args) throws Exception {
+        System.out.println("开始解析主合同信息。。。。。");
+        List<ContractExcelInfo> contractList = read_total();
+        System.out.println("完成解析主合同信息。。。。。");
+
+        System.out.println("开始解析签署方信息。。。。。");
+        List<ContractExcelInfo> userList = read_write_user();
+        System.out.println("完成解析签署方信息。。。。。");
+
+        System.out.println("开始解析合同属性信息。。。。。");
+        List<ContractExcelInfo> attrList = read_attr_info();
+        System.out.println("完成解析合同属性信息。。。。。");
+
+        objectCopy(contractList, userList, USER_START, USER_END);
+        objectCopy(contractList, attrList, ATTR_START, ATTR_END);
+
+        //处理null值
         for (Field field : ContractExcelInfo.class.getDeclaredFields()) {
             String fieldName = field.getName();
             PropertyDescriptor descriptor = new PropertyDescriptor(fieldName, ContractExcelInfo.class);
@@ -71,13 +79,11 @@ public class ReadExcel02 {
 
             for (ContractExcelInfo info : contractList) {
                 String value = String.valueOf(readMethod.invoke(info));
-                if(Objects.isNull(value) || "null".equalsIgnoreCase(value)){
+                if (Objects.isNull(value) || "null".equalsIgnoreCase(value)) {
                     writeMethod.invoke(info, "");
                 }
             }
         }
-
-        System.out.println("解析结果： " + JSON.toJSONString(contractList));
 
         CreateExcelV2 createExcel = new CreateExcelV2(contractList, "hello_world", false);
 
@@ -91,8 +97,7 @@ public class ReadExcel02 {
     }
 
     private static List<ContractExcelInfo> read_total() throws Exception {
-        String path = "D:/document/2022-02/合同信息01/0210_01_2.xlsx";
-        Workbook workbook = WorkbookFactory.create(new File(path));
+        Workbook workbook = WorkbookFactory.create(new File(TOTAL_PATH));
 
         Sheet sheet = workbook.getSheetAt(0);
         List<ContractExcelInfo> list = new ArrayList<>();
@@ -121,48 +126,45 @@ public class ReadExcel02 {
     }
 
 
-    private static List<UserInfo> read_write_user() throws Exception {
-        String path = "D:/document/2022-02/合同信息01/0210_02_2.xlsx";
-        Workbook workbook = WorkbookFactory.create(new File(path));
+    private static List<ContractExcelInfo> read_write_user() throws Exception {
+        Workbook workbook = WorkbookFactory.create(new File(USER_PATH));
 
         Sheet sheet = workbook.getSheetAt(0);
-        List<UserInfo> list = new ArrayList<>();
+
+        Map<String, ContractExcelInfo> userMap = new HashMap<>();
 
         for (int i = 1; i < sheet.getLastRowNum(); i++) {
             Row row = sheet.getRow(i);
 
-            double cttId = row.getCell(1).getNumericCellValue();
+            String cttId = String.valueOf(Double.valueOf(row.getCell(1).getNumericCellValue()).intValue());
+            //主体方、合作方
+            String userType = ExcelUtil.readAsString(2, row);
             //甲方、乙方
             String userTypeInfo = ExcelUtil.readAsString(3, row);
             //甲方名称、乙方名称
             String userTypeName = ExcelUtil.readAsString(4, row);
 
-            UserInfo userInfo = new UserInfo();
-            userInfo.setCttId(String.valueOf(Double.valueOf(cttId).intValue()));
-            userInfo.setUserType(userTypeInfo);
-            userInfo.setUserTypeName(userTypeName);
-            list.add(userInfo);
+            ContractExcelInfo info = userMap.getOrDefault(cttId, new ContractExcelInfo());
+            info.setVal0(cttId);
+            if ("主体方".equals(userType)) {
+                switch (userTypeInfo){
+                    case "甲方": info.setVal100(userTypeName); break;
+                    case "乙方": info.setVal101(userTypeName); break;
+                    case "丙方": info.setVal102(userTypeName); break;
+                }
+            }else {
+                switch (userTypeInfo){
+                    case "甲方": info.setVal103(userTypeName); break;
+                    case "乙方": info.setVal104(userTypeName); break;
+                    case "丙方": info.setVal105(userTypeName); break;
+                }
+            }
+
+            userMap.put(cttId, info);
         }
 
-        List<UserInfo> list2 = new ArrayList<>();
-        list.stream().collect(Collectors.groupingBy(var0 -> var0.getCttId()))
-                .forEach((parId, list0) -> {
-                    UserInfo userInfo = new UserInfo();
-                    userInfo.setCttId(parId);
-
-                    for (UserInfo info : list0) {
-
-                        if ("甲方".equals(info.getUserType())) {
-                            userInfo.setUserTypeName(info.getUserTypeName());
-                        } else {
-                            userInfo.setUserTypeName2(info.getUserTypeName());
-                        }
-                    }
-                    list2.add(userInfo);
-                });
-        list2.sort(Comparator.comparing(UserInfo::getCttId));
         workbook.close();
-        return list2;
+        return userMap.values().stream().collect(Collectors.toList());
     }
 
     private static List<ContractExcelInfo> read_attr_info() throws Exception {
@@ -177,8 +179,7 @@ public class ReadExcel02 {
             methodMap.put(headerName, writeMethod);
         }
 
-        String path = "D:/document/2022-02/合同信息01/0210_04_2.xlsx";
-        Workbook workbook = WorkbookFactory.create(new File(path));
+        Workbook workbook = WorkbookFactory.create(new File(ATTR_PATH));
 
         Sheet sheet = workbook.getSheetAt(0);
 
@@ -198,29 +199,11 @@ public class ReadExcel02 {
             }
             attrMap.put(cttId, info);
         }
+        workbook.close();
         return attrMap.values().stream().collect(Collectors.toList());
     }
 
-    @Data
-    public static class UserInfo {
-        //合同主键
-        private String cttId;
-
-        //类型：甲方、乙方
-        private String userType;
-
-        //甲方名称
-        private String userTypeName = "";
-
-        //乙方名称
-        private String userTypeName2 = "";
-    }
-
-    /*
-    主键	机构名称	合同名称	合同编码	合同分类	合同状态	变更状态	签约状态	合同来源	是否框架协议
-    合同签约时间	合同生效开始时间	合同生效结束时间	是否用章	用章类型	归档状态	合同创建人
-    合同创建时间	申请时间	审批完成时间
-     */
+    //根据导出的excel实时生成的对象，列数不固定
     @Data
     public static class ContractExcelInfo {
         @ExcelInfo(headerName = "合同主键")
@@ -249,85 +232,101 @@ public class ReadExcel02 {
         private String val11;
         @ExcelInfo(headerName = "合同生效结束时间")
         private String val12;
-        @ExcelInfo(headerName = "是否用章")
+        @ExcelInfo(headerName = "合同终止日期")
         private String val13;
-        @ExcelInfo(headerName = "用章类型")
+        @ExcelInfo(headerName = "是否用章")
         private String val14;
-        @ExcelInfo(headerName = "归档状态")
+        @ExcelInfo(headerName = "用章类型")
         private String val15;
-        @ExcelInfo(headerName = "合同创建人")
+        @ExcelInfo(headerName = "归档状态")
         private String val16;
-        @ExcelInfo(headerName = "合同创建时间")
+        @ExcelInfo(headerName = "合同创建人")
         private String val17;
-        @ExcelInfo(headerName = "申请时间")
+        @ExcelInfo(headerName = "合同创建时间")
         private String val18;
-        @ExcelInfo(headerName = "审批完成时间")
+        @ExcelInfo(headerName = "申请时间")
         private String val19;
-        @ExcelInfo(headerName = "合同金额")
+        @ExcelInfo(headerName = "审批完成时间")
         private String val20;
-        @ExcelInfo(headerName = "是否标准合同")
+        @ExcelInfo(headerName = "是否发起申请")
         private String val21;
-        @ExcelInfo(headerName = "结算方式")
+        @ExcelInfo(headerName = "合同金额")
         private String val22;
+        @ExcelInfo(headerName = "是否标准合同")
+        private String val23;
+        @ExcelInfo(headerName = "结算方式")
+        private String val24;
+        @ExcelInfo(headerName = "订单结算周期")
+        private String val25;
+        @ExcelInfo(headerName = "订单结算周期间隔日")
+        private String val26;
+        @ExcelInfo(headerName = "收支类型")
+        private String val27;
+        @ExcelInfo(headerName = "关联合同")
+        private String val28;
+        @ExcelInfo(headerName = "合同来源")
+        private String val29;
+        @ExcelInfo(headerName = "变更终止状态")
+        private String val30;
+        @ExcelInfo(headerName = "变更原因")
+        private String val31;
+        @ExcelInfo(headerName = "业务类型")
+        private String val32;
 
-        @ExcelInfo(headerName = "甲方")
+        @ExcelInfo(headerName = "主体方-甲方")
         private String val100;
-        @ExcelInfo(headerName = "乙方")
+        @ExcelInfo(headerName = "主体方-乙方")
         private String val101;
+        @ExcelInfo(headerName = "主体方-丙方")
+        private String val102;
+        @ExcelInfo(headerName = "相对方-甲方")
+        private String val103;
+        @ExcelInfo(headerName = "相对方-乙方")
+        private String val104;
+        @ExcelInfo(headerName = "相对方-丙方")
+        private String val105;
 
-        @ExcelInfo(headerName = "增值-签约选项")
-        private String val201;
-        @ExcelInfo(headerName = "增值-业务类型")
-        private String val202;
-        @ExcelInfo(headerName = "增值-是否关联交易方")
-        private String val203;
-        @ExcelInfo(headerName = "增值-付款方式")
-        private String val204;
-        @ExcelInfo(headerName = "增值-是否合同范本")
-        private String val205;
-        @ExcelInfo(headerName = "增值-是否已提示付款")
-        private String val206;
-        @ExcelInfo(headerName = "增值-合同状态")
-        private String val207;
-        @ExcelInfo(headerName = "增值-合同类型")
-        private String val208;
-        @ExcelInfo(headerName = "增值-所属条线")
-        private String val209;
-        @ExcelInfo(headerName = "增值-履约保证金")
-        private String val210;
-        @ExcelInfo(headerName = "增值-合同金额")
-        private String val211;
-        @ExcelInfo(headerName = "增值-合同期限（月）")
-        private String val212;
-        @ExcelInfo(headerName = "增值-月租金/月合同额（元）")
-        private String val213;
-        @ExcelInfo(headerName = "增值-城市")
-        private String val214;
-        @ExcelInfo(headerName = "增值-合作单位公司名称")
-        private String val215;
-        @ExcelInfo(headerName = "增值-项目名称")
-        private String val216;
-        @ExcelInfo(headerName = "增值-合同简要信息")
-        private String val217;
         @ExcelInfo(headerName = "增值-本合同金额")
-        private String val218;
+        private String val201;
+        @ExcelInfo(headerName = "增值-城市")
+        private String val202;
+        @ExcelInfo(headerName = "增值-付款方式")
+        private String val203;
         @ExcelInfo(headerName = "增值-公司地址")
-        private String val219;
+        private String val204;
+        @ExcelInfo(headerName = "增值-合同简要信息")
+        private String val205;
+        @ExcelInfo(headerName = "增值-合同金额")
+        private String val206;
+        @ExcelInfo(headerName = "增值-合同类型")
+        private String val207;
+        @ExcelInfo(headerName = "增值-合同期限（月）")
+        private String val208;
+        @ExcelInfo(headerName = "增值-合同状态")
+        private String val209;
+        @ExcelInfo(headerName = "增值-合作单位公司名称")
+        private String val210;
+        @ExcelInfo(headerName = "增值-履约保证金")
+        private String val211;
+        @ExcelInfo(headerName = "增值-签约选项")
+        private String val212;
         @ExcelInfo(headerName = "增值-使用途径")
-        private String val220;
+        private String val213;
+        @ExcelInfo(headerName = "增值-是否关联交易方")
+        private String val214;
+        @ExcelInfo(headerName = "增值-是否合同范本")
+        private String val215;
+        @ExcelInfo(headerName = "增值-是否已提示付款")
+        private String val216;
+        @ExcelInfo(headerName = "增值-所属条线")
+        private String val217;
         @ExcelInfo(headerName = "增值-物业资源编号")
+        private String val218;
+        @ExcelInfo(headerName = "增值-项目名称")
+        private String val219;
+        @ExcelInfo(headerName = "增值-业务类型")
+        private String val220;
+        @ExcelInfo(headerName = "增值-月租金/月合同额（元）")
         private String val221;
-        @ExcelInfo(headerName = "城市")
-        private String val222;
-        @ExcelInfo(headerName = "递增值")
-        private String val223;
-        @ExcelInfo(headerName = "合同金额（元）")
-        private String val224;
-        @ExcelInfo(headerName = "合同期限（月）")
-        private String val225;
-        @ExcelInfo(headerName = "合同文本")
-        private String val226;
-        @ExcelInfo(headerName = "是否框架合同")
-        private String val227;
     }
 }
