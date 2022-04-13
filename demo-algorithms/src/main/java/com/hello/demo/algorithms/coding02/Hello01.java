bf3a2273dbbe1e2fb404b4f2c7eaf7d716bc5e2f
zhw-wei <2261842924@qq.com>
2022-04-14 00:33:23 +0800

    coding2初始化

---
+++ demo-algorithms/src/main/java/com/hello/demo/algorithms/coding02/Hello01.java
@@ -0,0 +1,21 @@
+package com.hello.demo.algorithms.coding02;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author zhw
+ * @date 2022/4/13 10:15 PM
+ */
+@DisplayName("算法02")
+public class Hello01 {
+    /**
+     *
+     */
+
+    @Test
+    @DisplayName("")
+    public void test01(){
+
+    }
+}
---
+++ demo-algorithms/src/main/java/com/hello/demo/algorithms/coding02/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * BV1eg411w7gn
+ * @author zhw
+ * @date 2022/4/13 10:12 PM
+ */
+package com.hello.demo.algorithms.coding02;
